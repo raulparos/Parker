875c190b95697d6cbb6cf966528f3bcd2fa65bd2
Raul Paros <raul.paros@gmail.com>
2018-03-07 23:00:50 +0200

    Application set-up

---
+++ parkerapi/src/main/java/com/parker/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.parker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @ResponseBody
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "Hello!";
+    }
+}
