f414c7570087de52773f590980a7e803efdec344
Raul Paros <raul.paros@gmail.com>
2018-04-18 22:12:08 +0300

    Added ReservationController

---
+++ parkerapi/src/main/java/com/parker/controller/parkingspot/ReservationContoller.java
@@ -0,0 +1,10 @@
+package com.parker.controller.parkingspot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/reservation")
+public class ReservationContoller {
+
+}
